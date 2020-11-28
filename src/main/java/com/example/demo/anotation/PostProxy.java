e974f5dcdfa9d2ad0c3bd6ded531c1bdf3b2ebfe
Iurie Railean <blackrailean@gmail.com>
2020-11-29 01:58:54 +0200

    add PostProxy annotation

--- src/main/java/com/example/demo/anotation/PostProxy.java
+++ src/main/java/com/example/demo/anotation/PostProxy.java
@@ -1,4 +1,9 @@
 package com.example.demo.anotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface PostProxy {
+
 }
